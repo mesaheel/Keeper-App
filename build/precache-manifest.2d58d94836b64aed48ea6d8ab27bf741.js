0e14f30ed6b75d485a6663f38a05a84e59706101
Sahil Ali <sahil@Sahils-MacBook-Air.local>
2022-06-10 20:31:46 +0530

    first commit

---
+++ build/precache-manifest.2d58d94836b64aed48ea6d8ab27bf741.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23f06a574739e6c99e292c3d5565e91f",
+    "url": "/Keeper-app/index.html"
+  },
+  {
+    "revision": "e1b017533e6e40d3693f",
+    "url": "/Keeper-app/static/js/2.8a02f188.chunk.js"
+  },
+  {
+    "revision": "cd167e49402b075931fb",
+    "url": "/Keeper-app/static/js/main.9ccbb50d.chunk.js"
+  },
+  {
+    "revision": "dd1051f0b09b1315230e",
+    "url": "/Keeper-app/static/js/runtime-main.19c413c2.js"
+  }
+]);
